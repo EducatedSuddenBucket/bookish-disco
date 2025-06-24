2c8fdb711e2062154bc48997388e66d9c82b90be
EducatedSuddenBucket <educatedsuddenbucket@gmail.com>
2025-06-24 21:58:09 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  mode: 'production',
+  resolve: { extensions: ['.js'] },
+};
